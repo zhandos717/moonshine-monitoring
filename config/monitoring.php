9551819c8b28e07619e2319458a1b9d39f2d45af
Zhandos <zhandos.zhandarbekov@gmail.com>
2024-05-02 23:19:21 +0500

    Добавил instance_name

--- config/monitoring.php
+++ config/monitoring.php
@@ -2,5 +2,17 @@
 
 return [
     'auto_menu'     => true,
-    'instance_name' => 'Dev server'
+    'instance_name' => env('MONITORING_INSTANCE_NAME', env('APP_NAME')),
+
+
+    'notifications' => [
+        'telegram' => ''
+    ],
+    /** You can enable or disable migrations here*/
+    'migrations'    => true,
+
+    /** Purge recorded data
+     * Supports PHP strtotime options like: '-1 day', '-2 hours', ...
+     */
+    'purge_before'  => '-1 day',
 ];
